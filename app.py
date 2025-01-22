3f0b51e3bb4a5ee318c85892723efb23b962037a
aaron <aaron@linux-mint-laptop.tailb3905e.ts.net>
2025-01-22 09:35:13 -0800

    improved comments

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+#Install neccessary libraries
 from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
+#Route for the index page.
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Route for the calculation page.
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    # Get first car details
+    #Get first car details
     first_car_year = int(request.form['first_car_year'])
     first_car_make = request.form['first_car_make']
     first_car_model = request.form['first_car_model']
@@ -16,7 +20,7 @@ def calculate():
     first_car_horsepower = float(request.form['first_car_horsepower'])
     first_power_weight_ratio = first_car_curb_weight / first_car_horsepower
     
-    # Get second car details
+    #Get second car details.
     second_car_year = int(request.form['second_car_year'])
     second_car_make = request.form['second_car_make']
     second_car_model = request.form['second_car_model']
@@ -24,7 +28,7 @@ def calculate():
     second_car_horsepower = float(request.form['second_car_horsepower'])
     second_power_weight_ratio = second_car_curb_weight / second_car_horsepower
     
-    # Calculate percentage difference
+    #Calculate percentage difference.
     percentage_difference = -1 * (second_power_weight_ratio - first_power_weight_ratio) / first_power_weight_ratio * 100
     performance_comparison_term = "better" if percentage_difference >= 0 else "worse"
     
